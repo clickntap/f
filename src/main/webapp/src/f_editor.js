047661926f23aa3270876806e00ff961b1cb90b9
Tonino Mendicino <tonino@clickntap.com>
2020-12-21 15:41:18 +0100

    added cleanHTML and needsCleanHTML

--- src/main/webapp/src/f_editor.js
+++ src/main/webapp/src/f_editor.js
@@ -1,5 +1,8 @@
 (function(){
-  function toolbarState(doc) {
+  function editorUpdate(doc) {
+    var item = f(doc).closest('.f-editor').item;
+    var observer = item.observer;
+    observer.disconnect();
     var toolbar = f(doc).closest('.f-editor').find('.f-toolbar');
     var bold = toolbar.find('.bold');
     if(bold.item) { if(document.queryCommandState('bold')) { bold.addClass('on'); } else { bold.removeClass('on'); } }
@@ -15,15 +18,95 @@
     if(justifyRight.item) { if(document.queryCommandState('justifyRight')) { justifyRight.addClass('on'); } else { justifyRight.removeClass('on'); } }
     var parser = new DOMParser();
     var body = parser.parseFromString(doc.innerHTML, "text/html").body;
-    body.normalize();
-    doc.innerHTML = body.innerHTML;
-    parseHTML(body, 0);
+    var html = cleanHTML(body);
+    if(needsCleanHTML(body) === true) {
+      doc.innerHTML = html;
+    }
+    observer.observe(item, {childList: true, subtree: true, characterData:true, attributes:true});
   }
-  function parseHTML(element,level) {
+  var htmlTags = ['div','b','font','br','ul','ol','li','i'];
+  var htmlCssNames = ['color','text-align'];
+  function needsCleanHTML(element) {
+    var needsClean = false;
+    try {
+      var cssTexts = element.style.cssText.split(';');
+      f(cssTexts).each(function(cssText) {
+        if(cssText.trim() != '') {
+          var cssName = cssText.split(':')[0].trim().toLowerCase();
+          if(cssName != 'color' && cssName != 'text-align') {
+            needsClean = true;
+          }
+        }
+      });
+    } catch(err) {
+    }
+    if(needsClean === true) {
+      return needsClean;
+    }
     element.childNodes.forEach(function(child){
-      console.log(level,child.nodeName,child.nodeType,child.nodeValue);
-      parseHTML(child,level+1);
+      if(needsClean === false) {
+        var tagFound = false;
+        f(htmlTags).each(function(htmlTag){
+          if(htmlTag == child.nodeName.toLowerCase()) {
+            tagFound = true;
+          }
+          if('#text' == child.nodeName.toLowerCase()) {
+            tagFound = true;
+          }
+        });
+        if(tagFound === false) {
+          needsClean = true;
+        } else {
+          needsClean = needsCleanHTML(child);
+        }
+      }
     });
+    return needsClean;
+  }
+  function cleanHTML(element, level) {
+    if(!level) {
+      level = 0;
+    }
+    var html = '';
+    element.childNodes.forEach(function(child){
+      var tagFound = false;
+      var tag = '';
+      f(htmlTags).each(function(htmlTag){
+        if(htmlTag == child.nodeName.toLowerCase()) {
+          tagFound = true;
+          var style = '';
+          if(child.style && child.style.color && child.style.color != '') {
+            style += 'color:'+child.style.color+';';
+          }
+          if(child.style.textAlign) {
+            style += 'text-align:'+child.style.textAlign+';';
+          }
+          if(child.getAttribute('color')) {
+            style += 'color:'+child.getAttribute('color')+';';
+          }
+          if(style == '') {
+            tag += '<'+htmlTag+'>';
+          } else {
+            tag += '<'+htmlTag+' style="'+style+'">';
+          }
+        }
+      });
+      if('#text' == child.nodeName.toLowerCase()) {
+        tagFound = true;
+        var text = child.nodeValue.trim();
+        tag += text;
+      }
+      tag += cleanHTML(child,level+1);
+      f(htmlTags).each(function(htmlTag){
+        if(htmlTag == child.nodeName.toLowerCase()) {
+          if(htmlTag != 'br' && htmlTag != 'li') {
+            tag += '</'+htmlTag+'>';
+          }
+        }
+      });
+      html += tag;
+    });
+    return html;
   }
   function editorOff(item) {
     f(item).removeClass('on').find('.f-item').removeClass('on');
@@ -42,35 +125,49 @@
           editorOff(item);
         }, 250);
       });
+      item.addEventListener('selectstart', function (event) {
+        setTimeout(function() {
+          var editor = f(event.target.parentNode).closest('.f-editor').item;
+          editorUpdate(doc.item);
+        },0);
+      });
+      item.addEventListener('paste', function (event) {
+        setTimeout(function() {
+          var editor = f(item).closest('.f-editor').item;
+          editorUpdate(doc.item);
+        },0);
+      });
+      var observer = new MutationObserver(function(mutations) {
+        try {
+          var editor = f(mutations[0].target.parentNode).closest('.f-editor').item;
+          editorUpdate(doc.item);
+        } catch(err) {
+        }
+      });
+      item.observer = observer;
+      observer.observe(item, {childList: true, subtree: true, characterData:true, attributes:true});
       doc.item.setAttribute('contenteditable', 'true');
-      doc.item.addEventListener('DOMSubtreeModified', function (event) {
-        setTimeout(function() {
-          toolbarState(doc.item);
-        }, 0);
-      });
-      doc.item.addEventListener('selectstart', function (event) {
-        setTimeout(function() {
-          toolbarState(doc.item);
-        }, 0);
-      });
-      doc.item.addEventListener('keyup', function (event) {
-        setTimeout(function() {
-          toolbarState(doc.item);
-        }, 0);
-      });
       var toolbar = f(item).find('.f-toolbar');
-      setTimeout(function() {
-        document.execCommand('enableInlineTableEditing');
-      }, 1000);
-      toolbar.find('.bold').on('click', function() { document.execCommand('bold'); doc.item.focus(); toolbarState(doc.item);});
-      toolbar.find('.italic').on('click', function() { document.execCommand('italic'); doc.item.focus(); toolbarState(doc.item);});
-      toolbar.find('.ordered-list').on('click', function() { document.execCommand('insertOrderedList'); doc.item.focus(); toolbarState(doc.item);});
-      toolbar.find('.justify-left').on('click', function() { document.execCommand('justifyLeft'); doc.item.focus(); toolbarState(doc.item);});
-      toolbar.find('.justify-center').on('click', function() { document.execCommand('justifyCenter'); doc.item.focus(); toolbarState(doc.item);});
-      toolbar.find('.justify-right').on('click', function() { document.execCommand('justifyRight'); doc.item.focus(); toolbarState(doc.item);});
+      toolbar.find('.bold').on('click', function() { document.execCommand('bold'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.italic').on('click', function() { document.execCommand('italic'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.ordered-list').on('click', function() { document.execCommand('insertOrderedList'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.justify-left').on('click', function() { document.execCommand('justifyLeft'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.justify-center').on('click', function() { document.execCommand('justifyCenter'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.justify-right').on('click', function() { document.execCommand('justifyRight'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.clear').on('click', function() { document.execCommand('removeFormat'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.red').on('click', function() { document.execCommand('foreColor',false,'#aa0000'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.green').on('click', function() { document.execCommand('foreColor',false,'#00aa00'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.blue').on('click', function() { document.execCommand('foreColor',false,'#0000aa'); doc.item.focus(); editorUpdate(doc.item);});
+      toolbar.find('.yellow').on('click', function() { document.execCommand('foreColor',false,'#aaaa00'); doc.item.focus(); editorUpdate(doc.item);});
       f(item).removeClass('f_editor').addClass('f-editor');
+      setTimeout(function() {
+        editorUpdate(doc.item);
+        doc.item.focus();
+      },0);
     });
     resolve(context);
   };
+  f().prototype().cleanHTML = cleanHTML;
+  f().prototype().needsCleanHTML = needsCleanHTML;
   f().uiPlugins().push(plugin);
 })();
