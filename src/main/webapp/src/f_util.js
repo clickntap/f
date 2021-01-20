8cbd88c676bd827d95091b57c18c801f8041d649
Tonino Mendicino <tonino@clickntap.com>
2021-01-20 19:44:25 +0100

    fix week utils

--- src/main/webapp/src/f_util.js
+++ src/main/webapp/src/f_util.js
@@ -167,17 +167,17 @@
     return this;
   };
   Date.prototype.prevWeek = function () {
-    var currentWeek = this.getWeek();
+    var currentDay = this.getDay();
     do {
       this.setDate(this.getDate() - 1)
-    } while(currentWeek == this.getWeek());
+    } while(currentDay != this.getDay());
     return this;
   };
   Date.prototype.nextWeek = function () {
-    var currentWeek = this.getWeek();
+    var currentDay = this.getDay();
     do {
       this.setDate(this.getDate() + 1)
-    } while(currentWeek == this.getWeek());
+    } while(currentDay != this.getDay());
     return this;
   };
   Date.prototype.dayStart = function () {
@@ -233,27 +233,6 @@
     this.setDate(1);
     this.getStartOfWeek();
     return this;
-  };
-  Date.prototype.getWeek = function (dowOffset) {
-    dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0;
-    var newYear = new Date(this.getFullYear(),0,1);
-    var day = newYear.getDay() - dowOffset;
-    day = (day >= 0 ? day : day + 7);
-    var daynum = Math.floor((this.getTime() - newYear.getTime() -
-    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
-    var weeknum;
-    if(day < 4) {
-      weeknum = Math.floor((daynum+day-1)/7) + 1;
-      if(weeknum > 52) {
-        nYear = new Date(this.getFullYear() + 1,0,1);
-        nday = nYear.getDay() - dowOffset;
-        nday = nday >= 0 ? nday : nday + 7;
-        weeknum = nday < 4 ? 1 : 53;
-      }
-    } else {
-      weeknum = Math.floor((daynum+day-1)/7);
-    }
-    return weeknum;
   };
   Date.prototype.format = function (options, language) {
     if (options) {
