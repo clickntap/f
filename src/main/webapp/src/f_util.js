6d3560e047dce95e6352b97be7fce106026cfb57
Tonino Mendicino <tonino@clickntap.com>
2021-01-20 18:56:18 +0100

    added week utils

--- src/main/webapp/src/f_util.js
+++ src/main/webapp/src/f_util.js
@@ -128,16 +128,16 @@
   };
   Date.prototype.prevMonth = function () {
     var currentMonth = this.getMonth();
-    while(currentMonth == this.getMonth()) {
+    do {
       this.setMonth(this.getMonth() - 1);
-    }
+    } while(currentMonth == this.getMonth());
     return this;
   };
   Date.prototype.nextMonth = function () {
     var currentMonth = this.getMonth();
-    while(currentMonth == this.getMonth()) {
+    do {
       this.setMonth(this.getMonth() + 1);
-    }
+    } while(currentMonth == this.getMonth());
     return this;
   };
   Date.prototype.changeMonth = function (n) {
@@ -153,12 +153,31 @@
     }
     return this;
   };
+  Date.prototype.changeWeek = function (n) {
+    if(n < 0) {
+      for(var j=n;j<0;j++) {
+        this.prevWeek();
+      }
+    }
+    if(n > 0) {
+      for(var j=0;j<n;j++) {
+        this.nextWeek();
+      }
+    }
+    return this;
+  };
   Date.prototype.prevWeek = function () {
-    this.setDate(this.getDate() - 7)
+    var currentWeek = this.getWeek();
+    do {
+      this.setDate(this.getDate() - 1)
+    } while(currentWeek == this.getWeek());
     return this;
   };
   Date.prototype.nextWeek = function () {
-    this.setDate(this.getDate() + 7)
+    var currentWeek = this.getWeek();
+    do {
+      this.setDate(this.getDate() + 1)
+    } while(currentWeek == this.getWeek());
     return this;
   };
   Date.prototype.dayStart = function () {
@@ -214,6 +233,27 @@
     this.setDate(1);
     this.getStartOfWeek();
     return this;
+  };
+  Date.prototype.getWeek = function (dowOffset) {
+    dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0;
+    var newYear = new Date(this.getFullYear(),0,1);
+    var day = newYear.getDay() - dowOffset;
+    day = (day >= 0 ? day : day + 7);
+    var daynum = Math.floor((this.getTime() - newYear.getTime() -
+    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    var weeknum;
+    if(day < 4) {
+      weeknum = Math.floor((daynum+day-1)/7) + 1;
+      if(weeknum > 52) {
+        nYear = new Date(this.getFullYear() + 1,0,1);
+        nday = nYear.getDay() - dowOffset;
+        nday = nday >= 0 ? nday : nday + 7;
+        weeknum = nday < 4 ? 1 : 53;
+      }
+    } else {
+      weeknum = Math.floor((daynum+day-1)/7);
+    }
+    return weeknum;
   };
   Date.prototype.format = function (options, language) {
     if (options) {
